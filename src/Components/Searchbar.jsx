23deba9f53a596bfc9eb6f0b088fc95cdff5a5fa
Mehedi Hasan <developerhasan99@gmail.com>
2023-01-31 21:37:45 +0600

    suggestion

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import { movieNames } from "../utils/movieNames";
 
 export default function ({ searchValue, setSearchValue }) {
+  const [suggestion, setSuggestion] = useState([]);
+
   const handleChange = (e) => {
-    setSearchValue(e.target.value);
-    console.log(searchValue);
+    let userData = e.target.value;
+
+    setSearchValue(userData);
+
+    let filterdMovies = [];
+
+    if (userData) {
+      filterdMovies = movieNames.filter((data) => {
+        //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+        return data
+          .toLocaleLowerCase()
+          .startsWith(userData.toLocaleLowerCase());
+      });
+    }
+
+    let slicedNames = filterdMovies.slice(0, 30);
+
+    let sortedNames = slicedNames.sort((a, b) => {
+      let step = 0;
+    });
+
+    setSuggestion(sortedNames);
   };
+
+  const suggestedElements = suggestion.map((value, index) => (
+    <li key={index}>{value}</li>
+  ));
 
   return (
     <form className="search_form">
@@ -13,20 +40,11 @@ export default function ({ searchValue, setSearchValue }) {
         value={searchValue}
         type="text"
         placeholder="Search for movie..."
-        autocomplete="off"
+        autoComplete="off"
       />
-      <ul className="suggestion">
-        <li>sugas sfs</li>
-        <li>sugas sfs</li>
-        <li>sugas sfs</li>
-        <li>sugas sfs</li>
-        <li>sugas sfs</li>
-        <li>sugas sfs</li>
-        <li>sugas sfs</li>
-        <li>sugas sfs</li>
-        <li>sugas sfs</li>
-        <li>sugas sfs</li>
-      </ul>
+      {suggestion.length > 0 && (
+        <ul className="suggestion">{suggestedElements}</ul>
+      )}
     </form>
   );
 }
